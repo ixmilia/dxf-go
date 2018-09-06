2e92545a1c868547bcca8eaeaaa63ee17ab2bd14
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-09-06 10:39:30 -0700

    add doc comments for AcadVersion

--- acadVersion.go
+++ acadVersion.go
@@ -1,28 +1,70 @@
 package dxf
 
+// AcadVersion represents the minimum version of AutoCAD that is expected to be able to read the file.
 type AcadVersion int
 
 const (
+	// Version1_0 corresponds to the value "MC0.0"
 	Version1_0 AcadVersion = iota
+
+	// Version1_2 corresponds to the value "AC1.2"
 	Version1_2
+
+	// Version1_40 corresponds to the value "AC1.40"
 	Version1_40
+
+	// Version2_05 corresponds to the value "AC1.50"
 	Version2_05
+
+	// Version2_10 corresponds to the value "AC2.10"
 	Version2_10
+
+	// Version2_21 corresponds to the value "AC2.21"
 	Version2_21
+
+	// Version2_22 corresponds to the value "AC2.22"
 	Version2_22
+
+	// Version2_5 corresponds to the value "AC1002"
 	Version2_5
+
+	// Version2_6 corresponds to the value "AC1003"
 	Version2_6
+
+	// R9 corresponds to the value "AC1004"
 	R9
+
+	// R10 corresponds to the value "AC1006"
 	R10
+
+	// R11 corresponds to the value "AC1009"
 	R11
+
+	// R12 corresponds to the value "AC1009"
 	R12
+
+	// R13 corresponds to the value "AC1012"
 	R13
+
+	// R14 corresponds to the value "AC1014"
 	R14
+
+	// R2000 corresponds to the value "AC1015"
 	R2000
+
+	// R2004 corresponds to the value "AC1018"
 	R2004
+
+	// R2007 corresponds to the value "AC1021"
 	R2007
+
+	// R2010 corresponds to the value "AC1024"
 	R2010
+
+	// R2013 corresponds to the value "AC1027"
 	R2013
+
+	// R2018 corresponds to the value "AC1032"
 	R2018
 )
 
@@ -44,7 +86,7 @@ func parseAcadVersion(val string) AcadVersion {
 		return Version2_22
 	case "AC1002":
 		return Version2_5
-	case "A1009":
+	case "AC1003":
 		return Version2_6
 	case "AC1004":
 		return R9
@@ -75,7 +117,7 @@ func parseAcadVersion(val string) AcadVersion {
 	}
 }
 
-func (v AcadVersion) ToHeaderVersion() string {
+func (v AcadVersion) String() string {
 	switch v {
 	case Version1_0:
 		return "MC0.0"
