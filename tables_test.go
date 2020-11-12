8f82d0a37a5b2f0cc8d38f1f7b993ae88e1d896c
Brett V. Forsgren <brett.forsgren@outlook.com>
2020-11-11 18:42:56 -0800

    test tables directly against code pairs

--- tables_test.go
+++ tables_test.go
@@ -1,14 +1,13 @@
 package dxf
 
 import (
-	"strings"
 	"testing"
 )
 
 func TestReadLayer(t *testing.T) {
-	drawing := parseTableItem(t, "LAYER", join(
-		"  2", "layer-name",
-	))
+	drawing := parseTableItem(t, "LAYER",
+		NewStringCodePair(2, "layer-name"),
+	)
 	assertEqInt(t, 1, len(drawing.Layers))
 	layer := drawing.Layers[0]
 	assertEqString(t, "layer-name", layer.Name)
@@ -19,15 +18,18 @@ func TestWriteLayer(t *testing.T) {
 	l.Name = "layer-name"
 	d := NewDrawing()
 	d.Layers = append(d.Layers, l)
-	actual := d.String()
-	assertContains(t, join(
-		"100", "AcDbSymbolTableRecord",
-		"100", "AcDbLayerTableRecord",
-		"  2", "layer-name",
-		" 70", "     0",
-		" 62", "     7",
-		"  6", "CONTINUOUS",
-	), actual)
+	actual, err := d.CodePairs()
+	if err != nil {
+		t.Error(err)
+	}
+	assertContainsCodePairs(t, []CodePair{
+		NewStringCodePair(100, "AcDbSymbolTableRecord"),
+		NewStringCodePair(100, "AcDbLayerTableRecord"),
+		NewStringCodePair(2, "layer-name"),
+		NewShortCodePair(70, 0),
+		NewShortCodePair(62, 7),
+		NewStringCodePair(6, "CONTINUOUS"),
+	}, actual)
 }
 
 func TestRoundTripLayer(t *testing.T) {
@@ -42,82 +44,85 @@ func TestRoundTripLayer(t *testing.T) {
 }
 
 func TestReadLayers(t *testing.T) {
-	drawing := parse(t, join(
+	drawing := parseFromCodePairs(t,
 		// section decl
-		"  0", "SECTION",
-		"  2", "TABLES",
+		NewStringCodePair(0, "SECTION"),
+		NewStringCodePair(2, "TABLES"),
 		// table decl
-		"  0", "TABLE",
-		"  2", "LAYER",
+		NewStringCodePair(0, "TABLE"),
+		NewStringCodePair(2, "LAYER"),
 		// item
-		"  0", "LAYER",
-		"  2", "layer-1",
+		NewStringCodePair(0, "LAYER"),
+		NewStringCodePair(2, "layer-1"),
 		// item
-		"  0", "LAYER",
-		"  2", "layer-2",
+		NewStringCodePair(0, "LAYER"),
+		NewStringCodePair(2, "layer-2"),
 		// end
-		"  0", "ENDTAB",
-		"  0", "ENDSEC",
-		"  0", "EOF",
-	))
+		NewStringCodePair(0, "ENDTAB"),
+		NewStringCodePair(0, "ENDSEC"),
+		NewStringCodePair(0, "EOF"),
+	)
 	assertEqInt(t, 2, len(drawing.Layers))
 	assertEqString(t, "layer-1", drawing.Layers[0].Name)
 	assertEqString(t, "layer-2", drawing.Layers[1].Name)
 }
 
 func TestReadTableWithHandle(t *testing.T) {
-	drawing := parse(t, join(
+	drawing := parseFromCodePairs(t,
 		// section decl
-		"  0", "SECTION",
-		"  2", "TABLES",
+		NewStringCodePair(0, "SECTION"),
+		NewStringCodePair(2, "TABLES"),
 		// table decl
-		"  0", "TABLE",
-		"  2", "VPORT",
-		"  5", "ABCD", // n.b., handle is on the table, not the table item
+		NewStringCodePair(0, "TABLE"),
+		NewStringCodePair(2, "VPORT"),
+		NewStringCodePair(5, "ABCD"), // n.b., handle is on the table, not the table item
 		// item
-		"  0", "VPORT",
-		"  2", "vport-name",
+		NewStringCodePair(0, "VPORT"),
+		NewStringCodePair(2, "vport-name"),
 		// end
-		"  0", "ENDTAB",
-		"  0", "ENDSEC",
-		"  0", "EOF",
-	))
+		NewStringCodePair(0, "ENDTAB"),
+		NewStringCodePair(0, "ENDSEC"),
+		NewStringCodePair(0, "EOF"),
+	)
 	assertEqInt(t, 1, len(drawing.ViewPorts))
 	assertEqString(t, "vport-name", drawing.ViewPorts[0].Name)
 }
 
 func TestUnsupportedTable(t *testing.T) {
-	drawing := parse(t, join(
-		"  0", "SECTION",
-		"  2", "TABLES",
-		"  0", "TABLE",
-		"  2", "UNSUPPORTED",
-		"  0", "UNSUPPORTED",
-		"  2", "unsupported-name",
-		"  0", "ENDTAB",
-		"  0", "TABLE",
-		"  2", "LAYER",
-		"  0", "LAYER",
-		"  2", "layer-name",
-		"  0", "ENDTAB",
-		"  0", "ENDSEC",
-		"  0", "EOF",
-	))
+	drawing := parseFromCodePairs(t,
+		NewStringCodePair(0, "SECTION"),
+		NewStringCodePair(2, "TABLES"),
+		NewStringCodePair(0, "TABLE"),
+		NewStringCodePair(2, "UNSUPPORTED"),
+		NewStringCodePair(0, "UNSUPPORTED"),
+		NewStringCodePair(2, "unsupported-name"),
+		NewStringCodePair(0, "ENDTAB"),
+		NewStringCodePair(0, "TABLE"),
+		NewStringCodePair(2, "LAYER"),
+		NewStringCodePair(0, "LAYER"),
+		NewStringCodePair(2, "layer-name"),
+		NewStringCodePair(0, "ENDTAB"),
+		NewStringCodePair(0, "ENDSEC"),
+		NewStringCodePair(0, "EOF"),
+	)
 	assertEqInt(t, 1, len(drawing.Layers))
 	assertEqString(t, "layer-name", drawing.Layers[0].Name)
 }
 
-func parseTableItem(t *testing.T, tableType, body string) (drawing Drawing) {
-	drawing = parse(t, join(
-		"  0", "SECTION",
-		"  2", "TABLES",
-		"  0", "TABLE",
-		"  2", tableType,
-		"  0", tableType,
-	)+"\r\n"+strings.TrimSpace(body)+"\r\n"+join(
-		"  0", "ENDTAB",
-		"  0", "ENDSEC",
-		"  0", "EOF",
-	))
+func parseTableItem(t *testing.T, tableType string, codePairs ...CodePair) (drawing Drawing) {
+	allPairs := []CodePair{
+		NewStringCodePair(0, "SECTION"),
+		NewStringCodePair(2, "TABLES"),
+		NewStringCodePair(0, "TABLE"),
+		NewStringCodePair(2, tableType),
+		NewStringCodePair(0, tableType),
+	}
+	allPairs = append(allPairs, codePairs...)
+	allPairs = append(allPairs,
+		NewStringCodePair(0, "ENDTAB"),
+		NewStringCodePair(0, "ENDSEC"),
+		NewStringCodePair(0, "EOF"),
+	)
+	drawing = parseFromCodePairs(t, allPairs...)
 	return
 }
